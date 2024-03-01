9bc8bcd14c35779e311d91aa1b59f5bdb2cad767
Rossetow <rafaelrosseto05@gmail.com>
2024-02-29 21:07:03 -0300

    update

--- models/order.ts
+++ models/order.ts
@@ -2,7 +2,7 @@ import { BasicOrder, Order, OrderWithDetails } from "../types/order";
 import { db } from "../dist/db";
 import { OkPacket, RowDataPacket } from "mysql2"
 
-export const create = (order:BasicOrder, callback: Function) => {
+export const create = (order: BasicOrder, callback: Function) => {
     const queryString = 'INSERT INTO tOrder (product_id, customer_id, product_quantity) VALUES (?, ?, ?)'
 
     db.query(
@@ -17,6 +17,22 @@ export const create = (order:BasicOrder, callback: Function) => {
 
             const insertId = (<OkPacket>result).insertId
             callback(null, insertId)
+        }
+    )
+}
+
+export const update = (order: Order, callback: Function) => {
+    const queryString = `UPDATE Order SET product_id=?, product_quantity=? WHERE order_id = ?`
+
+    db.query(
+        queryString,
+        [
+            order.product.id,
+            order.quantity,
+            order.orderId
+        ],
+        (err, result) => {
+            if(err) { callback(err) }
         }
     )
 }
