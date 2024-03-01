eefb05c18eb9832d66062ceef24f40228e8e58cf
Rossetow <rafaelrosseto05@gmail.com>
2024-03-01 19:33:28 -0300

    criando os routes pra order

--- routes/orderRouter.ts
+++ routes/orderRouter.ts
@@ -2,4 +2,36 @@ import express, { Request, Response }from 'express'
 import * as orderModel from "../models/order"
 import { Order, BasicOrder } from "../types/order"
 
-const orderRouter = express.Router()
+const orderRouter = express.Router();
+
+orderRouter.get('/', async(req: Request, res:Response) => {
+    orderModel.findAll((err: Error, orders: Order[]) => {
+        if (err) {
+            return res.status(500).json({"errorMessage": err.message})
+        }
+
+        res.status(200).json({"dataOrders": orders})
+    })
+})
+
+orderRouter.post('/', async(req: Request, res:Response) => {
+    const newOrder: BasicOrder = req.body
+    orderModel.create(newOrder, (err: Error, order_id:number)=> {
+        if (err) {
+            return res.status(500).json({"errorMessage": err.message})
+        }
+
+        res.status(200).json({"order_id":order_id})
+    })
+})
+
+orderRouter.get('/:id', async(req: Request, res:Response) => {
+    const orderId:number = Number(req.params.id)
+    orderModel.findOne(orderId, (err: Error, order: Order) => {
+        if (err) {
+            return res.status(500).json({"errorMessage": err.message})
+        }
+
+        res.status(200).json({"dataOrders": order})
+    })
+})
