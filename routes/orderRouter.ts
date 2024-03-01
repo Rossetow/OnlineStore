364cdb902221c1147b5ffc5e463b9d1906a2f816
Rossetow <rafaelrosseto05@gmail.com>
2024-02-29 21:45:04 -0300

    iniciando o router

--- routes/orderRouter.ts
+++ routes/orderRouter.ts
@@ -1,2 +1,5 @@
 import express, { Request, Response }from 'express'
 import * as orderModel from "../models/order"
+import { Order, BasicOrder } from "../types/order"
+
+const orderRouter = express.Router()
